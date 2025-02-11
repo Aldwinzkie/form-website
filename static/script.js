ae0f66df8d576949101dd3324e3f0e70049fb3ed
Aldwinzkie <aldwinrendonzkie@gmail.com>
2025-02-11 18:49:59 +0800

    added restrictions to stop submitting when the input is invalid

--- static/script.js
+++ static/script.js
@@ -6,13 +6,14 @@ const input2 = document.querySelector("#pass:invalid");
 
 button.addEventListener('click', ()=> {
     if(confirm_pass.value != password.value){
-        input1.setAttribute('style','border-color:red;')
-        input2.setAttribute('style','border-color:red;')
+        input1.setAttribute('style','border-color:red;');
+        input2.setAttribute('style','border-color:red;');
     }else if(confirm_pass.value == password.value){
-        input1.setAttribute('style','border-color:blue;')
-        input2.setAttribute('style','border-color:blue;')
+        input1.setAttribute('style','border-color:blue;');
+        input2.setAttribute('style','border-color:blue;');
+        event.preventDefault()
     }else{
-        input1.setAttribute('style','border-color: hsl(0, 0%, 70%);')
-        input2.setAttribute('style','border-color: hsl(0, 0%, 70%);')
+        input1.setAttribute('style','border-color: hsl(0, 0%, 70%);');
+        input2.setAttribute('style','border-color: hsl(0, 0%, 70%);');
     }
 })
