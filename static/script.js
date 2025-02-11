3470ba6eca7731b6f0bd561a0b31b4b2aac7f4e4
Aldwinzkie <aldwinrendonzkie@gmail.com>
2025-02-11 18:41:07 +0800

    added the needed files for form website

---
+++ static/script.js
@@ -0,0 +1,18 @@
+const password = document.querySelector('#pass');
+const confirm_pass = document.querySelector('#con-pass');
+const button = document.querySelector(".button-sub");
+const input1 = document.querySelector("#con-pass:invalid");
+const input2 = document.querySelector("#pass:invalid");
+
+button.addEventListener('click', ()=> {
+    if(confirm_pass.value != password.value){
+        input1.setAttribute('style','border-color:red;')
+        input2.setAttribute('style','border-color:red;')
+    }else if(confirm_pass.value == password.value){
+        input1.setAttribute('style','border-color:blue;')
+        input2.setAttribute('style','border-color:blue;')
+    }else{
+        input1.setAttribute('style','border-color: hsl(0, 0%, 70%);')
+        input2.setAttribute('style','border-color: hsl(0, 0%, 70%);')
+    }
+})
